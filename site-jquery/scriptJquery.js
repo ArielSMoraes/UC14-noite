3eda04ce35bd9a89e5911fd0f2eedcb55c1dfe47
Ariel <ariel.s.moraes@gmail.com>
2017-08-08 21:08:47 -0300

    update para lazy load burro

--- site-jquery/scriptJquery.js
+++ site-jquery/scriptJquery.js
@@ -7,31 +7,31 @@ function handleClickMenu() {
     $(".title").text(textoMenu);
 }
 
-$(".main-content-row button").click(handleContent);
+//Requisições AJAX, com promises
 
-function handleContent() {
-    $(".main-content-row > div").removeClass("active-content")
-    $(this).parent().addClass("active-content");
-}
+$.get("https://swapi.co/api/starships/")
+    .then(geraListaDeNaves)
+    .then(getNext)
+    .then(geraListaDeNaves)
+    .then(getNext)
+    .then(geraListaDeNaves)
 
-$("#content2-open").click(function() {
-    $("#content2").modal({
-        fadeDuration: 500,
-        fadeDelay: 1
-    });
-})
-
-//Requisições AJAX
-
-$.get("https://swapi.co/api/starships/", function(data) {
+function geraListaDeNaves(data) {
     $(data.results).each(function(index) {
         var nave = gerarNave(this, index);
         $(".main-content-row").append(nave);
     });
     var modal = gerarModal();
     $("body").append(modal);
-});
+    return data.next
+}
 
+function getNext(next) {
+    return $.get(next);
+}
+
+
+// Modais
 function gerarModal() {
     var modal = `
         <div id="content1" style="display:none;">
@@ -43,8 +43,8 @@ function gerarModal() {
 function gerarNave(ship, index) {
     var nave = `<div class=${index}>
         <h4>${ship.name}</h4>
-        <span class="peso">Modelo: ${ship.model} </span>
-        <span class="altura">passageiros: ${ship.passengers} </span>
+        <span class="peso"><strong>Modelo:</strong> ${ship.model} </span>
+        <span class="altura"><strong>passageiros:</strong> ${ship.passengers} </span>
         <button><a href="#content1" rel="modal:open"> READ MORE </a></button>
     </div>`;
     return nave;
