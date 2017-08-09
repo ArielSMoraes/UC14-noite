857007bb8c1cce95877484e2d5816a481c05634a
Ariel <ariel.s.moraes@gmail.com>
2017-08-09 19:25:24 -0300

    Adicionando arquivos da aplicação dos correios

---
+++ script.js
@@ -0,0 +1,12 @@
+var correiosEnd = "http://correiosapi.apphb.com/cep/76873274";
+
+var respostaJson = {
+    "cep": "76873274",
+    "tipoDeLogradouro": "Rua",
+    "logradouro": "Juriti",
+    "bairro": "Setor 02",
+    "cidade": "Ariquemes",
+    "estado": "RO"
+}
+
+$("#cep").append(respostaJson.cep);
