f3f6ca6bc1f74f641101685c210ffc147cd339de
Ariel <ariel.s.moraes@gmail.com>
2017-08-09 21:52:48 -0300

    Aula dos correios

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var correiosEnd = "http://correiosapi.apphb.com/cep/76873274";
+var cep1 = {
+    "cep": "11111111",
+    "tipoDeLogradouro": "Rua",
+    "logradouro": "Lougradouro do 1",
+    "bairro": "Cidade Baixa",
+    "cidade": "Porto Alegre",
+    "estado": "RS"
+}
 
-var respostaJson = {
-    "cep": "76873274",
+var cep2 = {
+    "cep": "22222222",
     "tipoDeLogradouro": "Rua",
     "logradouro": "Juriti",
     "bairro": "Setor 02",
@@ -9,4 +16,40 @@ var respostaJson = {
     "estado": "RO"
 }
 
-$("#cep").append(respostaJson.cep);
+var cep3 = {
+    "cep": "33333333",
+    "tipoDeLogradouro": "Avenida",
+    "logradouro": "Jacupira",
+    "bairro": "Arangonhonha",
+    "cidade": "Macapa",
+    "estado": "AM"
+}
+
+function buscaCep(cep) {
+    if (cep == 11111111) {
+        return cep1;
+    }
+    if (cep == 22222222) {
+        return cep2;
+    }
+    if (cep == 33333333) {
+        return cep3;
+    }
+}
+
+$("#executarBusca").click(filtrarCep);
+
+function filtrarCep() {
+    var valorFiltro = $("#busca").val();
+    var cepFiltrado = buscaCep(valorFiltro);
+    mostraCepsNaTela(cepFiltrado);
+}
+
+function mostraCepsNaTela(cepFiltrado) {
+    $("#cep").text("cep: " + cepFiltrado.cep);
+    $("#tlog").text("Tipo de logradouro: " + cepFiltrado.tipoDeLogradouro);
+    $("#log").text("logradouro: " + cepFiltrado.logradouro);
+    $("#bairro").text("Bairro: " + cepFiltrado.bairro);
+    $("#cidade").text("Cidade: " + cepFiltrado.cidade);
+    $("#estado").text("Estado: " + cepFiltrado.estado);
+}
